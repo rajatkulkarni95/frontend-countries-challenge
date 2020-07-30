e6b0215981cb4736995ef90a61f1337eaabfde82
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2020-07-30 09:29:43 +0530

    Probable fixes to build

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { AiOutlineSearch } from "react-icons/ai";
+
 
 export const Search = ({ handleChange, value }) => (
   <SearchStyle
